3b94a7c3cc16e5531b25a7f52d3f1412539a5a51
StickyMango <alessandrodidomenicodev@gmail.com>
2022-12-16 12:25:05 +0100

    first test with ui

--- app/src/main/java/com/example/telewear/presentation/MainActivity.kt
+++ app/src/main/java/com/example/telewear/presentation/MainActivity.kt
@@ -9,24 +9,26 @@ package com.example.telewear.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
+import org.drinkless.td.libcore.telegram.Client
+import org.drinkless.td.libcore.telegram.TdApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.*
 import com.example.telewear.R
 import com.example.telewear.presentation.theme.TelewearTheme
-import org.drinkless.td.libcore.telegram.Client;
-import org.drinkless.td.libcore.telegram.Client.ResultHandler
-import org.drinkless.td.libcore.telegram.TdApi
+
 
 class ResultHandler : Client.ResultHandler{
     override fun onResult(`object`: TdApi.Object?) {
@@ -46,18 +48,80 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         val client =  Client.create(ResultHandler(), ExceptionHandler(), ExceptionHandler())
         setContent {
-            WearApp("Android")
+            TelewearTheme {
+
+                // Set three buttons -> contacts/+/settings
+                // Set Contacts list
+                //  for each contact in contact list, build a message card with avatar, name and preview of the last message
+                    ChatCard(ChatData("Alessandro", "Lorem Ipsum"))
+                }
+            }
         }
+    }
+
+data class ChatData(val contactName : String, val lastMessage : String){
+    internal val date : Int = 2022
+}
+
+data class IconProperties
+
+@Composable
+fun MainButton(){
+    Button(onClick = { /*TODO*/ },
+        modifier = Modifier
+            .padding()
+            .background(MaterialTheme.colors.background)){
+        Icon(imageVector = ImageVector.vectorResource(id = R.drawable.ic_settings_button),
+            contentDescription = "Gear Icon",
+            modifier = Modifier.size(ButtonDefaults.SmallButtonSize) )
     }
 }
 
 @Composable
+fun ChatCard(message : ChatData){
+    Row(modifier = Modifier.padding(all = 16.dp),
+        horizontalArrangement = Arrangement.Center) {
+        Image(
+            painter = painterResource(R.drawable.mango),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = message.contactName
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = message.lastMessage
+            )
+        }
+    }
+}
+
+@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
+@Composable
+fun AppPreview()
+{
+    ChatCard(ChatData("Alessandro", "Lorem Ipsum"))
+}
+
+/*
+@Composable
 fun WearApp(greetingName: String) {
     TelewearTheme {
-        /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
+        */
+/* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
          * version of LazyColumn for wear devices with some added features. For more information,
          * see d.android.com/wear/compose.
-         */
+         *//*
+
         Column(
                 modifier = Modifier
                         .fillMaxSize()
@@ -83,4 +147,4 @@ fun Greeting(greetingName: String) {
 @Composable
 fun DefaultPreview() {
     WearApp("Preview Android")
-}
+}*/
